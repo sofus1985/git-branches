19db5df4dbb85f5b1a0266cb7a9a7f2b8b6769cb
Sofiane Acherir <132833027+sofus1985@users.noreply.github.com>
2024-12-25 17:25:36 -0600

    commit changes branche featuresMovie

---
+++ src/test/java/test_home_page_movies/TestHomePageMovies.java
@@ -0,0 +1,17 @@
+package test_home_page_movies;
+
+import Base.Base;
+import home_page_movies.HomePageMovies;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestHomePageMovies extends Base {
+    @Test
+    public void testMovies(){
+        HomePageMovies homePageMovies = new HomePageMovies();
+        homePageMovies.clickButtonMovies();
+
+        Assert.assertTrue(homePageMovies.text.isDisplayed());
+    }
+
+}
